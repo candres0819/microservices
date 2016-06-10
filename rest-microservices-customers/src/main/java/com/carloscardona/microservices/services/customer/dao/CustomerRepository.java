d9a986d76e8f61bb120515a7c068446f983846b6
Carlos Andrés Cardona M <candres0819@gmail.com>
2016-06-10 01:30:43 -0500

    Signed-off-by: Carlos Andrés Cardona M <candres0819@gmail.com>

--- rest-microservices-customers/src/main/java/com/carloscardona/microservices/services/customer/dao/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.carloscardona.microservices.services.customer.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.carloscardona.microservices.services.customer.model.Customer;
-
-public interface CustomerRepository extends CrudRepository<Customer, Long> {
-
-}
