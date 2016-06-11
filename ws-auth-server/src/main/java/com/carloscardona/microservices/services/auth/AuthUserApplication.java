512271e209d91bfecae134465b8220619d14ce70
Carlos Andrés Cardona M <candres0819@gmail.com>
2016-06-11 01:29:17 -0500

    Signed-off-by: Carlos Andrés Cardona M <candres0819@gmail.com>

---
+++ ws-auth-server/src/main/java/com/carloscardona/microservices/services/auth/AuthUserApplication.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package com.carloscardona.microservices.services.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * The Main Spring Boot Application class that starts the authorization server.</br>
+ * </br>
+ * 
+ * Note that the server is also a Eureka client so as to register with the Eureka server and be auto-discovered by other Eureka clients.
+ *
+ * @author rohitghatol
+ */
+
+@ComponentScan
+@EnableAutoConfiguration
+@EnableEurekaClient
+public class AuthUserApplication {
+
+	/**
+	 * The main method.
+	 *
+	 * @param args
+	 *            the arguments
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(AuthUserApplication.class, args);
+	}
+}
